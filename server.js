fd4934616f226a3239baafaaa3ad6d62a16c7670
anuroopmallick <anuroopmallick1@gmail.com>
2025-07-27 00:13:21 +0530

    fixed cors

--- server.js
+++ server.js
@@ -7,11 +7,18 @@ const theatreRouter = require("./routes/theatreRoutes");
 const showRouter = require("./routes/showRoutes");
 const bookingRouter = require("./routes/bookingRoutes");
 const requestLoggerMiddleWare = require("./middleware/loggerMiddleware");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(requestLoggerMiddleWare);
+app.use(
+  cors({
+    origins: ["https://bookmyshow-server-s56s.onrender.com"],
+    credentials: true,
+  })
+);
 
 app.use("/api/users", userRouter);
 app.use("/api/movies", movieRouter);
